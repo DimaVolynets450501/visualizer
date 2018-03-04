7d6d184dcd05dc674db8a8525cf6e51af55f54c8
DimaVolynets450501 <dimanvolina@gmail.com>
2018-03-04 22:54:22 +0000

    added global config

---
+++ conf/config.py
@@ -0,0 +1,1 @@
+ROOT_PROJECT_PATH = ""
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+import os
+from utils.uci import test_package, UCI_page
+from utils.project import get_absolute_path
+import conf.config as cfg
 
-from utils import uci_parser
+def setup_root_path():
+    cfg.ROOT_PROJECT_PATH = get_absolute_path()
 
-# print("This is main file")
-uci_parser.test_package()
+if __name__ == "__main__":
+    setup_root_path()
+    # UCI_page.print()
+    test_package()
